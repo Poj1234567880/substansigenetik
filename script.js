2a0af44822bbcf0adfd542a17906240d3bc60502
Poj1234567880 <153247320+Poj1234567880@users.noreply.github.com>
2023-12-14 16:36:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 function transcribe() {
     var inputDNA = document.getElementById("inputDNA").value.toUpperCase();
-    var displayDNA = document.getElementById("displayDNA");
-    var displayNonsenseDNA = document.getElementById("displayNonsenseDNA");
     var displaymRNA = document.getElementById("displaymRNA");
-
-    // Transcribe DNA to DNA Nonsense
-    var nonsenseDNA = inputDNA.replace(/A|T|G|C/g, function(match) {
-        switch (match) {
-            case 'A':
-                return 'T';
-            case 'T':
-                return 'A';
-            case 'G':
-                return 'C';
-            case 'C':
-                return 'G';
-            default:
-                return match;
-        }
-    });
+    var displayAminoAcids = document.getElementById("displayAminoAcids");
 
     // Transcribe DNA to mRNA
     var mRNA = inputDNA.replace(/A|T|G|C/g, function(match) {
@@ -36,7 +19,38 @@ function transcribe() {
         }
     });
 
-    displayDNA.textContent = inputDNA;
-    displayNonsenseDNA.textContent = nonsenseDNA;
+    // Transcribe mRNA to Amino Acids using the genetic code table
+    var aminoAcidsMap = {
+        'UUU': 'Phe', 'UUC': 'Phe',
+        'UUA': 'Leu', 'UUG': 'Leu',
+        'CUU': 'Leu', 'CUC': 'Leu', 'CUA': 'Leu', 'CUG': 'Leu',
+        'AUU': 'Ile', 'AUC': 'Ile', 'AUA': 'Ile',
+        'AUG': 'Met',
+        'GUU': 'Val', 'GUC': 'Val', 'GUA': 'Val', 'GUG': 'Val',
+        'UCU': 'Ser', 'UCC': 'Ser', 'UCA': 'Ser', 'UCG': 'Ser',
+        'CCU': 'Pro', 'CCC': 'Pro', 'CCA': 'Pro', 'CCG': 'Pro',
+        'ACU': 'Thr', 'ACC': 'Thr', 'ACA': 'Thr', 'ACG': 'Thr',
+        'GCU': 'Ala', 'GCC': 'Ala', 'GCA': 'Ala', 'GCG': 'Ala',
+        'UAU': 'Tyr', 'UAC': 'Tyr',
+        'UAA': 'Stop', 'UAG': 'Stop', 'UGA': 'Stop',
+        'CAU': 'His', 'CAC': 'His',
+        'CAA': 'Gln', 'CAG': 'Gln',
+        'AAU': 'Asn', 'AAC': 'Asn',
+        'AAA': 'Lys', 'AAG': 'Lys',
+        'GAU': 'Asp', 'GAC': 'Asp',
+        'GAA': 'Glu', 'GAG': 'Glu',
+        'UGU': 'Cys', 'UGC': 'Cys',
+        'UGG': 'Trp',
+        'CGU': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg', 'CGG': 'Arg',
+        'AGU': 'Ser', 'AGC': 'Ser',
+        'AGA': 'Arg', 'AGG': 'Arg',
+        'GGU': 'Gly', 'GGC': 'Gly', 'GGA': 'Gly', 'GGG': 'Gly'
+    };
+
+    var aminoAcids = mRNA.match(/.{1,3}/g).map(function(codon) {
+        return aminoAcidsMap[codon] || '???';
+    }).join(' ');
+
     displaymRNA.textContent = mRNA;
+    displayAminoAcids.textContent = aminoAcids;
 }
